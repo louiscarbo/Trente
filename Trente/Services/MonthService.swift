200c2f6baea3f3fa60447955d8ad12d27bd5a9ba
Louis Carbo Estaque <louis@carbo-estaque.fr>
2025-08-03 22:53:03 +0200

    Fix order in create method

--- Trente/Services/MonthService.swift
+++ Trente/Services/MonthService.swift
@@ -14,7 +14,7 @@ final class MonthService {
 
     func create(month: Month, in context: ModelContext) throws {
         context.insert(month)
-        try context.save()
         try RecurringTransactionService.shared.refreshInstances(for: month, in: context)
+        try context.save()
     }
 }
